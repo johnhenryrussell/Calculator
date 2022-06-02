65c03ba85ef9d1fceead5efa30b5c7764ec6f9bf
John Russell <johnhenryrussell1@gmail.com>
2022-06-01 22:05:58 -0400

    Add javascript code to complete two compute two numbers

--- js-file.js
+++ js-file.js
@@ -1,9 +1,13 @@
 const display = document.querySelector(".display");
 const plus = document.querySelector("#add");
 const numberButtons = document.querySelectorAll(".save");
-let a = "";
-let b = "";
+const clearButton = document.querySelector("#clear");
+const operatorButtons = document.querySelectorAll(".arithmetic");
+const equalSign = document.querySelector("#equal");
+let num1 = "";
+let ab = [];
 let operation = "";
+let result = [];
 
 
 //---------- Arithmetic Functions---------- //
@@ -22,13 +26,13 @@ function divide(a,b) {
 }
 
 function operate(operator, a, b) {
-  if (operator === "add") {
+  if (operator === "+") {
     return add(a, b);
-  } else if (operator === "subtract") {
+  } else if (operator === "-") {
     return subtract(a, b);
-  } else if (operator === "multiply") {
+  } else if (operator === "x") {
     return multiply(a, b);
-  } else if (operator === "divide") {
+  } else if (operator === "/") {
     return divide(a, b);
   } else {
     return "ERROR"
@@ -39,14 +43,58 @@ function operate(operator, a, b) {
 
 //-------------Function to change Display------------//
 
-function addClick() {
+function addSaveValueClick() {
   for (let i=0; i<numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', saveValue);
   };
 };
 
+function addClearDisplayClick() {
+  clearButton.addEventListener('click', clearDisplay)
+};
+
+function clearDisplay() {
+  display.textContent = "";
+};
+
 function saveValue(e) {
   console.log(e.target.value);
+  num1 += e.target.value;
+  display.textContent = num1;
+};
+
+function addOperationClick() {
+  for (i=0; i<operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', chooseOperation)
+  };
+};
+
+function computer() {
+  equalSign.addEventListener('click', performCalculation);
+};
+
+function chooseOperation(e) {
+  console.log(e.target.value)
+  operation = e.target.value;
+  ab.push(num1);
+  num1="";
+  console.log(ab);
+};
+
+function performCalculation() {
+  ab.push(num1);
+  console.log(ab);
+  a = parseInt(ab[0]);
+  b = parseInt(ab[1])
+  let result = operate(operation, a, b);
+  display.textContent = result;
+  ab = [];
+  num1 = "";
 }
 
-addClick();
+//--------------Program-------------------------------//
+
+addSaveValueClick();
+addClearDisplayClick();
+addOperationClick();
+computer();
