ff49822dd6c3ef4e1f15754cf93f2c546a285ecb
John Russell <johnhenryrussell1@gmail.com>
2022-06-03 12:15:04 -0400

    Finish JS and add CSS styles

--- js-file.js
+++ js-file.js
@@ -4,8 +4,10 @@ const numberButtons = document.querySelectorAll(".save");
 const clearButton = document.querySelector("#clear");
 const operatorButtons = document.querySelectorAll(".arithmetic");
 const equalSign = document.querySelector("#equal");
+const decimalPoint = document.querySelector("#decimal");
+const negSign = document.querySelector("#neg");
 let num1 = "";
-let ab = [];
+let numberArray = [];
 let operation = "";
 let result = [];
 
@@ -13,16 +15,16 @@ let result = [];
 //---------- Arithmetic Functions---------- //
 
 function add(a, b) {
-  return a + b;
+  return Math.round((a+b) *100000) /100000;
 }
 function subtract(a, b) {
-  return a-b;
+  return Math.round((a-b) *100000) /100000;
 }
 function multiply(a, b) {
-  return a*b;
+  return Math.round((a*b) *100000) /100000;
 }
 function divide(a,b) {
-  return a/b;
+  return Math.round((a/b) *100000) /100000;
 }
 
 function operate(operator, a, b) {
@@ -43,11 +45,43 @@ function operate(operator, a, b) {
 
 //-------------Function to change Display------------//
 
-function addSaveValueClick() {
+function addClickToButtons() {
   for (let i=0; i<numberButtons.length; i++) {
-    numberButtons[i].addEventListener('click', saveValue);
+    numberButtons[i].addEventListener('click', addToArray);
   };
 };
+
+function addToArray(e) {
+  num1+=e.target.value;
+  console.log(num1);
+  display.textContent = num1;
+}
+
+//-----------Function to add decimal point----------------------//
+
+function addDecimalClick() {
+  decimalPoint.addEventListener('click', addDecimal) 
+};
+
+function addDecimal() {
+  decimalPoint.disabled = true;
+  num1 += ".";
+  display.textContent = num1;
+}
+
+
+//-----------Function to make number negative---------------------//
+
+function addNegativeClick() {
+  negSign.addEventListener('click', addNegative)
+};
+
+function addNegative() {
+  num1 *= -1;
+  display.textContent = num1;
+}
+
+//-----------Function to Clear Display and Variables------------//
 
 function addClearDisplayClick() {
   clearButton.addEventListener('click', clearDisplay)
@@ -55,13 +89,23 @@ function addClearDisplayClick() {
 
 function clearDisplay() {
   display.textContent = "";
+  numberArray = [];
+  num1 = "";
 };
 
-function saveValue(e) {
-  console.log(e.target.value);
-  num1 += e.target.value;
-  display.textContent = num1;
-};
+function pushToArray() {
+  numberArray.push(num1);
+  numberArray.push(operationValue);
+  num1 = "";
+}
+
+function performOperation() {
+  numberArray[0] = parseFloat(numberArray[0]);
+  numberArray[2] = parseFloat(numberArray[2]);
+  return result = operate(numberArray[1], numberArray[0], numberArray[2]);
+}
+
+//-------Function to choose operation----------//
 
 function addOperationClick() {
   for (i=0; i<operatorButtons.length; i++) {
@@ -69,32 +113,51 @@ function addOperationClick() {
   };
 };
 
-function computer() {
+function chooseOperation(e) {
+  decimalPoint.disabled = false;
+  operationValue = e.target.value;
+  if (numberArray.length < 4) {
+    pushToArray();
+  console.log(numberArray);
+  } else {
+      pushToArray();
+      performOperation();
+      numberArray.splice(0, 3, result);
+      display.textContent = result;
+      console.log(numberArray);
+  }
+}
+
+//----------Function to perform calculation--------//
+
+function addEqualSignClick() {
   equalSign.addEventListener('click', performCalculation);
 };
 
-function chooseOperation(e) {
-  console.log(e.target.value)
-  operation = e.target.value;
-  ab.push(num1);
-  num1="";
-  console.log(ab);
-};
-
 function performCalculation() {
-  ab.push(num1);
-  console.log(ab);
-  a = parseInt(ab[0]);
-  b = parseInt(ab[1])
-  let result = operate(operation, a, b);
-  display.textContent = result;
-  ab = [];
-  num1 = "";
+  decimalPoint.disabled = false;
+  if (numberArray.length > 3) {
+    numberArray.push(num1);
+    num1="";
+    performOperation();       
+    numberArray.splice(0, 3, result);
+    performOperation();
+    console.log(result);
+    display.textContent = result;
+
+  } else {
+    numberArray.push(num1);
+    performOperation();
+    display.textContent = result;
+  }
+
 }
 
 //--------------Program-------------------------------//
 
-addSaveValueClick();
+addClickToButtons();
+addDecimalClick();
+addNegativeClick();
 addClearDisplayClick();
 addOperationClick();
-computer();
+addEqualSignClick();
